2a32ab40818630b828ce505e05cb109718d5936a
Dhanasekar <svdhanasekar@outlook.com>
2024-07-29 21:51:05 +0100

    Initial commit wit catalog code

---
+++ src/main/java/com/dvlabs/spring_boot_microservice_course/SpringBootMicroserviceCourseApplication.java
@@ -0,0 +1,12 @@
+package com.dvlabs.spring_boot_microservice_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicroserviceCourseApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dvlabs/spring_boot_microservice_course/SpringBootMicroserviceCourseApplicationTests.java
@@ -0,0 +1,11 @@
+package com.dvlabs.spring_boot_microservice_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceCourseApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
